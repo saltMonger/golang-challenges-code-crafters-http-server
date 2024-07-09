d44df70d368feeaee7d1b3bdda7796f10b945c4b
Sean Robb <SRobb@ctdi.com>
2024-07-09 11:26:00 -0400

    also remove uncommented comment, whoops

--- app/server.go
+++ app/server.go
@@ -10,8 +10,6 @@ import (
 func main() {
 	// You can use print statements as follows for debugging, they'll be visible when running tests.
 	fmt.Println("Logs from your program will appear here!")
-
-	Uncomment this block to pass the first stage
 	
 	l, err := net.Listen("tcp", "0.0.0.0:4221")
 	if err != nil {
